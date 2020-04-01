bad9de1641746801cd8a3ee9a89040716edab102
SignorAutoma <fabio.caputo996@gmail.com>
2020-04-01 20:07:19 +0200

    Init second_assignment branch and RIOT device working

---
+++ RIOT_Clients/emcute_mqttsn/bin/native/auto_init_gnrc_netif/auto_init_stm32_eth.d
@@ -0,0 +1,8 @@
+/home/signorautoma/RIOT/examples/emcute_mqttsn/bin/native/auto_init_gnrc_netif/auto_init_stm32_eth.o: \
+ /home/signorautoma/RIOT/sys/auto_init/netif/auto_init_stm32_eth.c \
+ /usr/include/stdc-predef.h \
+ /home/signorautoma/RIOT/examples/emcute_mqttsn/bin/native/riotbuild/riotbuild.h
+
+/usr/include/stdc-predef.h:
+
+/home/signorautoma/RIOT/examples/emcute_mqttsn/bin/native/riotbuild/riotbuild.h:
---
+++ RIOT_Clients/emcute_mqttsn/bin/native/core/clist.d
@@ -0,0 +1,16 @@
+/home/signorautoma/RIOT/examples/emcute_mqttsn/bin/native/core/clist.o: \
+ /home/signorautoma/RIOT/core/clist.c /usr/include/stdc-predef.h \
+ /home/signorautoma/RIOT/examples/emcute_mqttsn/bin/native/riotbuild/riotbuild.h \
+ /home/signorautoma/RIOT/core/include/clist.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /home/signorautoma/RIOT/core/include/list.h
+
+/usr/include/stdc-predef.h:
+
+/home/signorautoma/RIOT/examples/emcute_mqttsn/bin/native/riotbuild/riotbuild.h:
+
+/home/signorautoma/RIOT/core/include/clist.h:
+
+/usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h:
+
+/home/signorautoma/RIOT/core/include/list.h:
